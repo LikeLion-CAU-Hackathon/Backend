9341b4cfdc56f806aee6ce4924adfee07adcfd25
jjjaehong <woghd3014@gmail.com>
2025-11-11 01:18:18 +0900

    chore: 테스트 코드 수정 테스트는 메모리모드로

--- src/test/java/com/example/santa/SantaApplicationTests.java
+++ src/test/java/com/example/santa/SantaApplicationTests.java
@@ -2,8 +2,10 @@ package com.example.santa;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class SantaApplicationTests {
 
 	@Test
